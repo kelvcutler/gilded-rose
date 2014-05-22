a98d64e9a62c482b1432e3c34c7776319b8f7cf2
Kelv Cutler <kcutler@ancestry.com>
2014-05-22 13:55:21 -0600

    My Solution!

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -1,29 +1,36 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace GildedRose.Console
 {
-    class Program
+    public class Program
     {
+        private const string Vest = "+5 Dexterity Vest";
+        private const string Elixir = "Elixir of the Mongoose";
+        private const string ConjuredManaCake = "Conjured Mana Cake";
+        private const string AgedBrie = "Aged Brie";
+        private const string BackstagePass = "Backstage passes to a TAFKAL80ETC concert";
+        private const string Sulfuras = "Sulfuras, Hand of Ragnaros";
+
         IList<Item> Items;
+
         static void Main(string[] args)
         {
-            System.Console.WriteLine("OMGHAI!");
-
             var app = new Program()
                           {
                               Items = new List<Item>
                                           {
-                                              new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                                              new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                                              new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                                              new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                                              new Item {Name = Vest, SellIn = 10, Quality = 20},
+                                              new Item {Name = AgedBrie, SellIn = 2, Quality = 0},
+                                              new Item {Name = Elixir, SellIn = 5, Quality = 7},
+                                              new Item {Name = Sulfuras, SellIn = 0, Quality = 80},
                                               new Item
                                                   {
-                                                      Name = "Backstage passes to a TAFKAL80ETC concert",
+                                                      Name = BackstagePass,
                                                       SellIn = 15,
                                                       Quality = 20
                                                   },
-                                              new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+                                              new Item {Name = ConjuredManaCake, SellIn = 3, Quality = 6}
                                           }
 
                           };
@@ -34,82 +41,64 @@ namespace GildedRose.Console
 
         }
 
+        public void SetItems(IList<Item> items)
+        {
+            Items = items;
+        }
+
         public void UpdateQuality()
         {
-            for (var i = 0; i < Items.Count; i++)
+            foreach (var item in Items)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (Items[i].Quality > 0)
-                    {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
-                    }
-                }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].SellIn < 11)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    Items[i].SellIn = Items[i].SellIn - 1;
-                }
-
-                if (Items[i].SellIn < 0)
-                {
-                    if (Items[i].Name != "Aged Brie")
-                    {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].Quality > 0)
-                            {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-                        }
-                    }
-                }
+                item.Quality += QualityDelta(item);
+                item.SellIn += SellInDelta(item);
+                if (item.Name != Sulfuras)
+                    item.Quality = Math.Max(0, Math.Min(item.Quality, 50));
             }
         }
 
+        private int SellInDelta(Item item)
+        {
+            switch (item.Name)
+            {
+                case Sulfuras:
+                    return 0;
+                case AgedBrie:
+                case BackstagePass:
+                case Vest:
+                case Elixir:
+                case ConjuredManaCake:
+                default:
+                    return -1;
+            }
+        }
+
+        private int QualityDelta(Item item)
+        {
+            switch (item.Name)
+            {
+                case Sulfuras:
+                    return 0;
+                case AgedBrie:
+                    return 1;
+                case BackstagePass:
+                    if (item.SellIn <= 0)
+                        return -1 * item.Quality;
+                    if (item.SellIn <= 5)
+                        return 3;
+                    if (item.SellIn <= 10)
+                        return 2;
+                    return 1;
+                case ConjuredManaCake:
+                    return -2;
+                case Vest:
+                case Elixir:
+                default:
+                    if (item.SellIn <= 0)
+                        return -2;
+                    return -1;
+            }
+        }
     }
 
     public class Item
--- src/GildedRose.Tests/TestAssemblyTests.cs
+++ src/GildedRose.Tests/TestAssemblyTests.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using GildedRose.Console;
 using NUnit.Framework;
 
 namespace GildedRose.Tests
@@ -5,10 +7,117 @@ namespace GildedRose.Tests
     [TestFixture]
     public class TestAssemblyTests
     {
+        private readonly Program _app = new Program();
+
         [Test]
         public void TestTheTruth()
         {
             Assert.IsTrue(true);
+        }
+
+        [TestCase("+5 Dexterity Vest", 10, 20, 10)]
+        [TestCase("Elixir of the Mongoose", 5, 7, 5)]
+        public void Normal_items_degrade_by_1(string itemName, int initialSellIn, int initialQuality, int runNTimes)
+        {
+            var items = BuildItemsAndRun(itemName, initialSellIn, initialQuality, runNTimes);
+            Assert.That(items[0].SellIn, Is.EqualTo(initialSellIn - runNTimes));
+            Assert.That(items[0].Quality, Is.EqualTo(initialQuality - runNTimes));
+        }
+
+        [TestCase("+5 Dexterity Vest", 10, 20, 30)]
+        [TestCase("Elixir of the Mongoose", 5, 7, 10)]
+        public void Quality_of_item_is_never_less_than_0(string itemName, int initialSellIn, int initialQuality, int runNTimes)
+        {
+            var items = BuildItemsAndRun(itemName, initialSellIn, initialQuality, runNTimes);
+            Assert.That(items[0].Quality, Is.GreaterThanOrEqualTo(0));
+        }
+
+        [TestCase("+5 Dexterity Vest", 10, 20, 15)]
+        [TestCase("Elixir of the Mongoose", 5, 10, 7)]
+        public void When_sell_by_date_is_passed_quality_degrades_by_2(string itemName, int initialSellIn, int initialQuality, int runNTimes)
+        {
+            var items = BuildItemsAndRun(itemName, initialSellIn, initialQuality, runNTimes);
+            int qualityAtEndOfSellByDate = initialQuality - initialSellIn;
+            int daysOverSellBy = runNTimes - initialSellIn;
+            Assert.That(items[0].Quality, Is.EqualTo(qualityAtEndOfSellByDate - 2 * daysOverSellBy));
+        }
+
+        [TestCase("Sulfuras, Hand of Ragnaros", 0, 80, 1)]
+        [TestCase("Sulfuras, Hand of Ragnaros", 0, 80, 50)]
+        public void Sulfuras_never_changes(string itemName, int initialSellIn, int initialQuality, int runNTimes)
+        {
+            var items = BuildItemsAndRun(itemName, initialSellIn, initialQuality, runNTimes);
+            Assert.That(items[0].Quality, Is.EqualTo(80));
+            Assert.That(items[0].SellIn, Is.EqualTo(0));
+        }
+
+        [TestCase("Aged Brie", 2, 0, 2)]
+        public void Aged_brie_increases_by_1(string itemName, int initialSellIn, int initialQuality, int runNTimes)
+        {
+            var items = BuildItemsAndRun(itemName, initialSellIn, initialQuality, runNTimes);
+            Assert.That(items[0].Quality, Is.EqualTo(initialQuality + runNTimes));
+        }
+
+        [TestCase("Aged Brie", 2, 0, 51)]
+        public void Aged_brie_never_grows_more_than_50(string itemName, int initialSellIn, int initialQuality, int runNTimes)
+        {
+            var items = BuildItemsAndRun(itemName, initialSellIn, initialQuality, runNTimes);
+            Assert.That(items[0].Quality, Is.EqualTo(50));
+        }
+
+        [TestCase("Backstage passes to a TAFKAL80ETC concert", 15, 20, 5)]
+        public void Backstage_passes_increase_by_1_with_more_than_10_days_left(string itemName, int initialSellIn, int initialQuality, int runNTimes)
+        {
+            var items = BuildItemsAndRun(itemName, initialSellIn, initialQuality, runNTimes);
+            Assert.That(items[0].Quality, Is.EqualTo(initialQuality+runNTimes));
+        }
+
+        [TestCase("Backstage passes to a TAFKAL80ETC concert", 10, 20, 5)]
+        public void Backstage_passes_increase_by_2_with_less_than_10_days_left(string itemName, int initialSellIn, int initialQuality, int runNTimes)
+        {
+            var items = BuildItemsAndRun(itemName, initialSellIn, initialQuality, runNTimes);
+            Assert.That(items[0].Quality, Is.EqualTo(initialQuality + 2 * runNTimes));
+        }
+
+        [TestCase("Backstage passes to a TAFKAL80ETC concert", 5, 20, 5)]
+        public void Backstage_passes_increase_by_3_with_less_than_10_days_left(string itemName, int initialSellIn, int initialQuality, int runNTimes)
+        {
+            var items = BuildItemsAndRun(itemName, initialSellIn, initialQuality, runNTimes);
+            Assert.That(items[0].Quality, Is.EqualTo(initialQuality + 3 * runNTimes));
+        }
+
+        [TestCase("Backstage passes to a TAFKAL80ETC concert", 1, 20, 2)]
+        public void Backstage_passes_quality_drops_to_0_after_sellby(string itemName, int initialSellIn, int initialQuality, int runNTimes)
+        {
+            var items = BuildItemsAndRun(itemName, initialSellIn, initialQuality, runNTimes);
+            Assert.That(items[0].Quality, Is.EqualTo(0));
+        }
+
+        [TestCase("Conjured Mana Cake", 3, 6, 3)]
+        [TestCase("Conjured Mana Cake", 6, 10, 4)]
+        public void Conjured_item_quality_decreases_by_2(string itemName, int initialSellIn, int initialQuality, int runNTimes)
+        {
+            var items = BuildItemsAndRun(itemName, initialSellIn, initialQuality, runNTimes);
+            Assert.That(items[0].Quality, Is.EqualTo(initialQuality - 2 * runNTimes));
+        }
+
+        private IList<Item> BuildItemsAndRun(string itemName, int initialSellIn, int initialQuality, int runNTimes)
+        {
+            var items = new List<Item>
+            {
+                new Item {Name = itemName, SellIn = initialSellIn, Quality = initialQuality},
+            };
+            _app.SetItems(items);
+            UpdateQuality(runNTimes);
+            return items;
+        }
+
+        private void UpdateQuality(int nTimes)
+        {
+            for (var i = 0; i < nTimes; i++)
+            {
+                _app.UpdateQuality();
+            }
         }
     }
 }
